8ea7f203200f8c69c2447052b06b63e0346e0af7
wusiyu <stephenwu2019@163.com>
2019-11-27 22:03:23 +0800

    order book title

--- src/components/home/OrderBook.vue
+++ src/components/home/OrderBook.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="ob">
-    <span>Order Book</span>
+    <span class="ob-title">Order Book</span>
   </div>
 </template>
 
 <style scoped>
-.ob{
+.ob {
   background: var(--container-bg);
   color: var(--page-text);
   width: 100%;
   height: 100%;
+  font-size: 12px;
+  position: relative;
+}
+
+.ob-title {
+  background-color: var(--container-top);
+  color: var(--page-text-highlight);
+  width: 100%;
+  box-sizing: border-box;
+  padding: 10px;
+  display: inline-block;
+  font-size: 16px;
+  position: absolute;
 }
 </style>
