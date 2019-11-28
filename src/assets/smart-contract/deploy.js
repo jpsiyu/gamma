1b1e9f27b97a1440b00e13a1aff871b136ad47d1
wusiyu <stephenwu2019@163.com>
2019-11-28 19:53:57 +0800

    add erc20 abi

--- src/assets/smart-contract/deploy.js
+++ src/assets/smart-contract/deploy.js
@@ -1,9 +1,13 @@
 import gammaAbi from './gammaAbi.json'
+import erc20Abi from './erc20Abi.json'
 
-const gammaAddress = '0xB1DCC402A8Ce472C4c59caaa4C7C709BeF0A10cC'
+const gammaAddr= '0xB1DCC402A8Ce472C4c59caaa4C7C709BeF0A10cC'
+const GTMAddr = '0xD5d3B39Bd6259579fB6885D69789aB05dC895Fc7'
 
 export default {
-  gammaAddress,
+  gammaAddr,
   gammaAbi,
+  erc20Abi,
+  GTMAddr,
 }
 
