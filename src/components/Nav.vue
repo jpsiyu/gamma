484ce4bf29db0c9fff052a7f39876001cea5bdf5
wusiyu <stephenwu2019@163.com>
2019-11-28 13:58:32 +0800

    deposit token

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,14 +9,18 @@
       </span>
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item v-for="(item, index) in pairs" :key="index" :command="item">{{item.coin}}</el-dropdown-item>
+        <el-dropdown-item command="other">Other</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
+    <DepositToken ref="deposit" />
   </div>
 </template>
 
 <script>
+import DepositToken from '@/components/popup/DepositToken'
 import { mapState } from 'vuex'
 export default {
+  components: { DepositToken },
   computed: {
     ...mapState({
       pairs: state => state.pairs,
@@ -25,8 +29,16 @@ export default {
   },
   methods: {
     handleCommand(item) {
+      if (item === 'other') {
+        this.handleOther()
+        return
+      }
       const path = item.coin + '_' + item.base
       this.$router.push({ path }).catch(() => { })
+    },
+
+    handleOther() {
+      this.$refs.deposit.show()
     }
   }
 }
