54e508456ce18ddfa3cd9568b09585915c0780ce
wusiyu <stephenwu2019@163.com>
2019-11-29 17:33:48 +0800

    sell order

--- src/components/home/Sell.vue
+++ src/components/home/Sell.vue
@@ -11,8 +11,8 @@
           <span slot="append">{{curPair.base}}</span>
         </el-input>
       </el-form-item>
-      <el-form-item label="Total" prop="total">
-        <el-input v-model="form.total" readonly></el-input>
+      <el-form-item label="Total">
+        <el-input v-model="total" readonly></el-input>
       </el-form-item>
       <el-form-item label="Expires" prop="expires">
         <el-input v-model="form.expires">
@@ -20,27 +20,75 @@
         </el-input>
       </el-form-item>
     </el-form>
-    <el-button type="danger" class="sell-button">Sell Order</el-button>
+    <el-button type="danger" class="sell-button" @click="sell">Sell Order</el-button>
+    <NotifyHash ref="notifyHash" />
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import BigNumber from 'bignumber.js'
+import NotifyHash from '@/components/popup/NotifyHash'
 export default {
+  components: { NotifyHash },
   data() {
     return {
       form: {
         amount: '',
         price: '',
-        total: '0.000',
         expires: '10000',
       }
     }
   },
   computed: {
     ...mapState({
+      account: state => state.account,
       curPair: state => state.curPair,
     }),
+    total() {
+      if (
+        !this.form.amount ||
+        isNaN(this.form.amount) ||
+        !this.form.price ||
+        isNaN(this.form.price)
+      ) return ''
+      return BigNumber(this.form.amount).multipliedBy(this.form.price).toFixed()
+    }
+  },
+  methods: {
+    sell() {
+      if (!this.total) return
+
+      const amountUnit = BigNumber(this.form.amount).multipliedBy(10 ** 18).toFixed()
+      const totalUnit = BigNumber(this.total).multipliedBy(10 ** 18).toFixed()
+
+      let curBlockNum = 0
+      let nonce = 0
+
+      Promise.all([
+        this.$gamma.web3.eth.getBlockNumber(),
+        this.$gamma.web3.eth.getTransactionCount(this.account),
+      ])
+        .then(res => {
+          curBlockNum = Number(res[0]) + Number(this.form.expires)
+          nonce = Number(res[1] + 1).toString()
+          //console.log('sell', this.$gamma.tokenAddr(), amountUnit, this.$gamma.ethAddr(), totalUnit, curBlockNum, nonce)
+          this.$gamma.dex.methods.sell(
+            this.$gamma.ethAddr(),
+            totalUnit,
+            this.$gamma.tokenAddr(),
+            amountUnit,
+            curBlockNum,
+            nonce,
+          ).send({ from: this.account })
+            .on('transactionHash', hash => {
+              this.$refs.notifyHash.show({ hashes: [hash] })
+            })
+            .on('receipt', receipt => {
+              console.log('receipt', receipt)
+            })
+        })
+    }
   }
 }
 </script>
